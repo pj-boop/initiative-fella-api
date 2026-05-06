38ae7c67f3a8ffb25fa39c725ce717210e25620e
pj <pjasong@gmail.com>
2026-05-06 19:44:07 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import job from "./lib/cron.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
